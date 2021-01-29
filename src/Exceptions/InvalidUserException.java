efcd01bb2bcad30a092129ea7eb0c1c80fe8f15f
Luís Vieira <48384492+30Massas@users.noreply.github.com>
2021-01-29 16:49:31 +0000

    Add files via upload

---
+++ src/Exceptions/InvalidUserException.java
@@ -0,0 +1,11 @@
+package Exceptions;
+
+public class InvalidUserException extends Exception {
+    public InvalidUserException() {
+        super();
+    }
+
+    public InvalidUserException(String m) {
+        super(m);
+    }
+}
